80363f8cc23a367eb89fe5ae4735d1ce78c3b603
Fedonko <tovarkav15@gmail.com>
2026-09-13 04:03:34 +1000

    Build FEDONKO VISIT responsive personal website

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('a[href^="#"]').forEach(link=>link.addEventListener('click',()=>{const target=document.querySelector(link.getAttribute('href'));if(target){target.setAttribute('tabindex','-1');target.focus({preventScroll:true});}}));
